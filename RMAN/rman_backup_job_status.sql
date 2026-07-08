638644df4681e2eac7970a8e333ff088070f886b
Vasith18 <133999370+Vasith18@users.noreply.github.com>
2026-07-08 12:41:31 +0530

    Create rman_backup_job_status.sql

---
+++ RMAN/rman_backup_job_status.sql
@@ -0,0 +1,9 @@
+SELECT
+    session_key,
+    input_type,
+    status,
+    TO_CHAR(start_time, 'DD-MON-YYYY HH24:MI') AS start_time,
+    TO_CHAR(end_time, 'DD-MON-YYYY HH24:MI') AS end_time,
+    ROUND(elapsed_seconds / 60, 2) AS duration_minutes
+FROM v$rman_backup_job_details
+ORDER BY start_time DESC;
